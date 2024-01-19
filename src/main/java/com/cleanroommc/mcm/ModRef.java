4815ef30a5ad58f1ad2d79b7c8ac00f760488e2c
strubium <113206902+strubium@users.noreply.github.com>
2024-01-19 17:07:24 -0600

    Build

--- src/main/java/com/cleanroommc/mcm/ModRef.java
+++ src/main/java/com/cleanroommc/mcm/ModRef.java
@@ -5,7 +5,7 @@ package com.cleanroommc.mcm;
  * <p>
  * ID and VERSION are automatically updated by RFG.
  */
-public class ModReference {
+public class ModRef {
     public static final String MODID = "mcm";
     public static final String NAME = "Mega Core Mod";
     public static final String VERSION = "1.0";
