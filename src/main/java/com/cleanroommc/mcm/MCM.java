3c74c0cee92d50067593448129d3a8b9a89f96c3
strubium <113206902+strubium@users.noreply.github.com>
2024-01-19 13:02:47 -0600

    Add ID imports

--- src/main/java/com/cleanroommc/mcm/MCM.java
+++ src/main/java/com/cleanroommc/mcm/MCM.java
@@ -4,6 +4,9 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import static com.cleanroommc.mcm.ModRef.ID;
+import static com.cleanroommc.mcm.ModRef.NAME;
+import static com.cleanroommc.mcm.ModRef.VERSION;
 
 @Mod(modid = ModRef.MODID, name = ModRef.NAME, version = ModRef.VERSION)
 public class MCM {
