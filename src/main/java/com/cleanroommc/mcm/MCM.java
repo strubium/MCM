d900fa4799b43a2c418e049233084fbfe6762402
strubium <113206902+strubium@users.noreply.github.com>
2024-01-19 13:06:26 -0600

    Wow

--- src/main/java/com/cleanroommc/mcm/MCM.java
+++ src/main/java/com/cleanroommc/mcm/MCM.java
@@ -15,17 +15,13 @@ public class MCM {
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent preInitializationEvent) {
-        commonProxy.preInit(this);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent initializationEvent) {
-        MWCRecipes.register();
-        commonProxy.init(this);
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent postInitializationEvent) {
-        commonProxy.postInit(this, postInitializationEvent);
     }
 }
