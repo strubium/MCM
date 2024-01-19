6eab38336fdf29618ba8de23a12733358eeb0086
strubium <113206902+strubium@users.noreply.github.com>
2024-01-19 12:52:54 -0600

    Create ModRef.java

---
+++ src/main/java/com/cleanroommc/mcm/ModRef.java
@@ -0,0 +1,14 @@
+package com.cleanroommc.mcm;
+
+
+/**
+ * This class defines constants for MWC.
+ * <p>
+ * ID and VERSION are automatically updated by RFG.
+ */
+public class ModReference {
+    public static final String MODID = "mcm";
+    public static final String NAME = "Mega Core Mod";
+    public static final String VERSION = "1.0";
+  
+}
