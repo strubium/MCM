4250fed96202c20edeecce983d7dcfd10a52dd69
strubium <113206902+strubium@users.noreply.github.com>
2024-01-19 12:49:51 -0600

    Create MCM.java

---
+++ src/main/java/com/cleanroommc/mcm/MCM.java
@@ -0,0 +1,26 @@
+package com.cleanroommc.mcm;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+
+@Mod(modid = ModRef.MODID, name = ModRef.NAME, ModRef = Constants.VERSION)
+public class MCM {
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent preInitializationEvent) {
+        commonProxy.preInit(this);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent initializationEvent) {
+        MWCRecipes.register();
+        commonProxy.init(this);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent postInitializationEvent) {
+        commonProxy.postInit(this, postInitializationEvent);
+    }
+}
