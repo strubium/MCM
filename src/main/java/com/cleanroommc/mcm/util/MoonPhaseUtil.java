415115693db7a2976268cfe03112f56f96563656
strubium <113206902+strubium@users.noreply.github.com>
2024-01-23 10:50:43 -0600

    Credit

--- src/main/java/com/cleanroommc/mcm/util/MoonPhaseUtil.java
+++ src/main/java/com/cleanroommc/mcm/util/MoonPhaseUtil.java
@@ -1,9 +1,19 @@
-/**
- * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
- * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+/*
+ * Source: Bookshelf -- MoonPhases
+ * Copyright (C) 2024 Darkhax
  *
- * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
- * 2.1.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 3
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.cleanroommc.mcm.util;
